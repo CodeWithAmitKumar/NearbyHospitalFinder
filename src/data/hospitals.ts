2695045964a6f253bf3e40b7fcdb01fe269ac152
codeWithAmitKumar <work.amitpatra@gmail.com>
2024-11-07 20:18:15 +0530

    add more data

--- src/data/hospitals.ts
+++ src/data/hospitals.ts
@@ -5,53 +5,106 @@ export const hospitals: Hospital[] = [
   {
     id: '1',
     name: 'Jyoti Hospital',
-    address: '123 Healthcare Ave, Medical District',
+    address: 'KURUDA, Balasore, Odisha 756056',
     city: 'Balasore',
-    distance: 0.8,  // in kilometers
+    distance: 3,  // in kilometers
     rating: 4.8,
     specialties: ['Emergency Care', 'Cardiology', 'Neurology'],
     emergency: true,
-    phone: '(555) 123-4567',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHsXcb8-6TXrjgtotUPzjhQylBJyu3FipCsA&s',
+    phone: '06782 256 375',
+    image: 'https://www.joonsquare.com/usermanage/image/business/jyothi-hospital-balasore-12183/jyothi-hospital-balasore-jyothi-hospital-004.jpg',
     waitTime: 15
   },
   {
     id: '2',
-    name: 'St. Mary\'s Medical Center',
-    address: '456 Wellness Blvd, Healing Quarter',
-    city: 'Boston',
-    distance: 1.2,  // in kilometers
-    rating: 4.6,
-    specialties: ['Pediatrics', 'Orthopedics', 'Oncology'],
+    name: 'District Headquarters Hospital (DHH) Balasore',
+    address: 'OT Road, Near Kacheri Bazaar, Balasore, Odisha 756001',
+    city: 'Balasore',
+    distance: 0.5,  // in kilometers
+    rating: 4.3,
+    specialties: ['Cardiology', 'Urology', 'Pulmonology'],
     emergency: true,
-    phone: '(555) 234-5678',
-    image: 'https://images.unsplash.com/photo-1632833239869-a37e3a5806d2?auto=format&fit=crop&q=80&w=1000',
-    waitTime: 25
+    phone: '6782 262 101',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2h2urZlhcI_w5udoMzJI_TEhx-jLvCnkmNg&s',
+    waitTime: 20
   },
   {
     id: '3',
-    name: 'Riverside Community Hospital',
-    address: '789 Care Lane, Riverside',
+    name: 'Balasore Bhadrak District Co-Operative Hospital',
+    address: 'OT Road, Remuna, Balasore, Odisha 756019',
     city: 'Chicago',
     distance: 2.5,  // in kilometers
     rating: 4.4,
-    specialties: ['Family Medicine', 'Surgery', 'Mental Health'],
-    emergency: false,
-    phone: '(555) 345-6789',
+    specialties: ['General medicine', 'gynecology', 'pediatrics', 'pathology'],
+    emergency: true,
+    phone: '+91 6782 241 489',
     image: 'https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?auto=format&fit=crop&q=80&w=1000',
-    waitTime: 40
+    waitTime: 25
   },
   {
     id: '4',
-    name: 'Metropolitan General Hospital',
-    address: '321 Health Street',
-    city: 'New York',
-    distance: 1.5,  // in kilometers
-    rating: 4.7,
-    specialties: ['Emergency Care', 'Surgery', 'Pediatrics'],
+    name: 'Apollo Clinic Balasore',
+    address: 'Near DIC Office, OT Road, Balasore, Odisha 756001',
+    city: 'Balasore',
+    distance: 10,  // in kilometers
+    rating: 3.7,
+    specialties: ['Emergency Care', 'diagnostics', 'Pediatrics'],
     emergency: true,
-    phone: '(555) 456-7890',
-    image: 'https://images.unsplash.com/photo-1586773860418-d37222d8fce3?auto=format&fit=crop&q=80&w=1000',
-    waitTime: 20
-  }
+    phone: '91 6782 242 611',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9ch1qmUuQ-gHVEqqH6w_VMAL7Re3EeuH12w&s',
+    waitTime: 10
+  },
+  {
+    id: '5',
+    name: 'Ayush Multi Specialty Hospital',
+    address: 'Vivekananda Marg, OT Road, Balasore, Odisha 756001',
+    city: 'Balasore',
+    distance: 7.5,  // in kilometers
+    rating: 2.4,
+    specialties: ['General medicine', 'gynecology', 'pediatrics'],
+    emergency: false,
+    phone: '91 83379 99999',
+
+    image: 'https://content.jdmagicbox.com/v2/comp/hyderabad/t7/040pxx40.xx40.190930122123.x9t7/catalogue/aayush-multi-speciality-hospital-hyderabad-ade9ylmsio.jpg',
+    waitTime: 7
+  },
+  {
+    id: '6',
+    name: 'Central Hospital',
+    address: 'OT Road, Remuna, Balasore, Odisha 756019',
+    city: 'Balasore',
+    distance: 2.5,  // in kilometers
+    rating: 4.4,
+    specialties: ['General medicine', 'gynecology', 'pediatrics', 'pathology'],
+    emergency: true,
+    phone: '+91 6782 241 160',
+    image: 'https://eangae.net/wp-content/uploads/2023/01/Jyoti-ICU.jpeg',
+    waitTime: 7
+  },
+  {
+    id: '7',
+    name: 'Red Cross Hospital',
+    address: 'Red Cross Building, OT Road, Near Gandhi Smruti Bhawan, Balasore, Odisha 756001',
+    city: 'Balasore',
+    distance: 5.5,  // in kilometers
+    rating: 3.2,
+    specialties: ['General medicine'],
+    emergency: false,
+    phone: '6782 262 024',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn8JN2YrJRP1P8zFLxDYHwQI-KCr0VZ6o00omqi9zhyUEO6du8-_AQ88mRhgaHfoqjSBQ&usqp=CAU',
+    waitTime: 13
+  },
+  {
+    id: '8',
+    name: 'Sun Hospital',
+    address: 'Near Fakir Mohan College Road, Balasore, Odisha 756001',
+    city: 'Balasore',
+    distance: 1.5,  // in kilometers
+    rating: 3.4,
+    specialties: [ 'gynecology', 'pediatrics'],
+    emergency: false,
+    phone: '6782 240 777',
+    image: 'https://content.jdmagicbox.com/comp/cuttack/87/0671p671std30087/catalogue/sun-hospital-pvt-ltd-tulsipur-cuttack-hospitals-uha4irv57j.jpg',
+    waitTime: 11
+  },
 ];
